e8ad465d406408a99dc203d5197ddef0eed9f1fe
mint <3041271982@qq.com>
2022-01-06 15:22:37 +0800

    gitTest third commit!

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -5,5 +5,7 @@
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("master change");
+        System.out.println("hot-fix change");
     }
 }
